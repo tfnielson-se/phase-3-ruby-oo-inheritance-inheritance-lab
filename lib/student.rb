3ebe625dcd719783fabbc88cd7c7bb9dc29ff12e
Thomas <tfnielson.se@gmail.com>
2023-02-01 12:36:36 -0700

    commit tree cad82c398b6f73b55d4fbcb2b1ddda5adca808fc

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,32 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        self.knowledge <<  knowledge
+    end
+
+    # def knowledge
+    #     @knowledge
+    # end
+
 
 end
+
+# class Student < User
+#     attr_reader :knowledge
+  
+#     def initialize
+#       @knowledge = []
+#     end
+  
+#     def learn(knowledge)
+#       @knowledge << knowledge
+#     end
+  
+#   end
